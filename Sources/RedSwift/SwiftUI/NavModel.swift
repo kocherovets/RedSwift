5335ef0628d6c9a52fe195812770bbcd1b771bb5
Dmitry Kocherovets <kocherovets@atlas.ru>
2021-08-06 00:08:06 +0300

    v3

---
+++ Sources/RedSwift/SwiftUI/NavModel.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+
+public protocol NavModel: Equatable {
+    var title: String? { get }
+    var rightBtn: NavRightBtn? { get }
+    func setNavigation(_ nBar: UINavigationBar, _ vcView: UIView)
+}
+
+public struct NavRightBtn: Equatable {
+    public let icon: UIImage?
+    public let reaction: Command
+
+    public init(icon: UIImage?, reaction: Command) {
+        self.icon = icon
+        self.reaction = reaction
+    }
+}
